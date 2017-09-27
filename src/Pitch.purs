b7b640d71c11957324516a7f55ac1673451dcd70
Matthew Leon <ml@matthewleon.com>
2017-09-27 17:56:04 -0400

    pitch maps

--- src/Pitch.purs
+++ src/Pitch.purs
@@ -2,10 +2,14 @@ module Pitch where
 
 import Prelude
 
-import Data.Enum (class BoundedEnum, class Enum, Cardinality(..), defaultPred, defaultSucc, fromEnum, toEnum)
+import Data.Array as Array
+import Data.Enum (class BoundedEnum, class Enum, Cardinality(..), defaultPred, defaultSucc, fromEnum, toEnum, enumFromTo)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(..))
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Tuple (Tuple(..))
 
 data Key = A | As | B | C | Cs | D | Ds | E | F | Fs | G | Gs
 derive instance genericKey :: Generic Key _
@@ -85,6 +89,107 @@ mkNote _    (Octave 8) = Nothing
 mkNote k  o            = Just $ Note k  o
 
 newtype Pitch = Pitch Number
+derive newtype instance showPitch :: Show Pitch
+derive newtype instance eqPitch   :: Eq Pitch
+derive newtype instance ordPitch  :: Ord Pitch
 
---notePitches :: Map Note Pitch
+notePitches :: Map Note Pitch
+notePitches = Map.fromFoldable $ Array.zipWith Tuple notes pitches
 
+pitchNotes :: Map Pitch Note
+pitchNotes = Map.fromFoldable $ Array.zipWith Tuple pitches notes
+
+notes :: Array Note
+notes = enumFromTo bottom top
+
+pitches :: Array Pitch
+pitches = Pitch <$> [
+    27.5
+  , 29.1352
+  , 30.8677
+  , 32.7032
+  , 34.6478
+  , 36.7081
+  , 38.8909
+  , 41.2034
+  , 43.6535
+  , 46.2493
+  , 48.9994
+  , 51.9131
+  , 55.0
+  , 58.2705
+  , 61.7354
+  , 65.4064
+  , 69.2957
+  , 73.4162
+  , 77.7817
+  , 82.4069
+  , 87.3071
+  , 92.4986
+  , 97.9989
+  , 103.826
+  , 110.000
+  , 116.541
+  , 123.471
+  , 130.813
+  , 138.591
+  , 146.832
+  , 155.563
+  , 164.814
+  , 174.614
+  , 184.997
+  , 195.998
+  , 207.652
+  , 220.000
+  , 233.082
+  , 246.942
+  , 261.626
+  , 277.183
+  , 293.665
+  , 311.127
+  , 329.628
+  , 349.228
+  , 369.994
+  , 391.995
+  , 415.305
+  , 440.000
+  , 466.164
+  , 493.883
+  , 523.251
+  , 554.365
+  , 587.330
+  , 622.254
+  , 659.255
+  , 698.456
+  , 739.989
+  , 783.991
+  , 830.609
+  , 880.000
+  , 932.328
+  , 987.767
+  , 1046.50
+  , 1108.73
+  , 1174.66
+  , 1244.51
+  , 1318.51
+  , 1396.91
+  , 1479.98
+  , 1567.98
+  , 1661.22
+  , 1760.00
+  , 1864.66
+  , 1975.53
+  , 2093.00
+  , 2217.46
+  , 2349.32
+  , 2489.02
+  , 2637.02
+  , 2793.83
+  , 2959.96
+  , 3135.96
+  , 3322.44
+  , 3520.00
+  , 3729.31
+  , 3951.07
+  , 4186.01
+]
